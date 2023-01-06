acacc747c96e856bdcf056087b80cd60ebd990b2
Indicaza <indicazas@gmail.com>
2023-01-05 21:42:43 -0500

    improved Page scaling, and lighthouse performance

--- src/components/pages/pages.js
+++ src/components/pages/pages.js
@@ -1,17 +1,17 @@
-export function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+// export function reveal() {
+//     var reveals = document.querySelectorAll(".reveal");
   
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+//     for (var i = 0; i < reveals.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
   
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
+//       if (elementTop < windowHeight - elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
   
-  window.addEventListener("scroll", reveal);
+//   window.addEventListener("scroll", reveal);
--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -18,7 +18,8 @@ import './pages.js';
 import './pages.css';
 
 
-export const Page = (props) => {
+export const Page = React.memo((props) => {
+  
   const pagesArray = [page1, page2, page5, page6];
   const randomIndex = Math.floor(Math.random() * pagesArray.length);
   const page = pagesArray[randomIndex];
@@ -35,76 +36,78 @@ export const Page = (props) => {
   
   return (
     <div ref={ref} className='container'>
+
         <div className={inView ? 'page fade-bottom grow' : 'page'} style={{backgroundImage:`url(${page})`}}>
-        <div className='sacredGeo grow' style={{backgroundImage:`url(${geo})`}}/>
-          <div  className='title grow'>
-            <h2 className='grow'>{props.name}</h2>
-            <div className='challenge grow'>
-                <h3>{props.meta}</h3>
-                <p>CR: {props.Challenge}</p>
+
+          <div className='sacredGeo' style={{backgroundImage:`url(${geo})`}}/>
+
+            <div  className='title'>
+              <h2>{props.name}</h2>
+              <div className='challenge'>
+                  <h3>{props.meta}</h3>
+                  <p>CR: {props.Challenge}</p>
+              </div>
             </div>
-          </div>
-          <div 
-          id="torn_edge_banner" 
-          className="picture torn_container torn_left torn_right grow"
-            style={{
-              backgroundImage:`url(${props.img_url})`,
-            }}
-          >
-          <div className='savingThrows grow'>
-            <p>Saving Throws: {props['Saving Throws']}</p>
-          </div>
-          </div>
-          <div className='details grow'>
-            <p className='grow'>Armor Class: {props['Armor Class']}</p>
-            <p className='grow'>Hit Points: {props['Hit Points']}</p>
-            <p className='grow'>Speed: {props.Speed}</p>
-          </div>
-          <div className='content'>
-            <div className='stats grow'>
-              <ul>
-                <li className='grow'>
-                  <p>STR: {props.STR} - {props.STR_mod}</p>
-                </li>
-                <li className='grow'>
-                  <p>DEX: {props.DEX} - {props.DEX_mod}</p>
-                </li>
-                <li className='grow'>
-                  <p>CON: {props.CON} - {props.CON_mod}</p>
-                </li>
-                <li className='grow'>
-                  <p>INT: {props.INT} - {props.INT_mod}</p>
-                </li>
-                <li className='grow'>
-                  <p>WIS: {props.WIS} - {props.WIS_mod}</p>
-                </li>
-                <li className='grow'>
-                  <p>CHA: {props.CHA} - {props.CHA_mod}</p>
-                </li>
-              </ul>
+            
+            <div className="picture grow"style={{backgroundImage:`url(${props.img_url})`}}>
+
+            <div className='savingThrows grow'>
+              <p>Saving Throws: {props['Saving Throws']}</p>
             </div>
-            <div className='body grow'>
-              <ul className='abilities'>
-                <h3>Ablilities</h3>
-                <li>
-                  <p>Languages: {props.Languages}</p>
-                </li>
-                <li>
-                  <p>Senses: {props.Senses}</p>
-                </li>
-                <li>
-                  <p>Skills: {props.Skills}</p>
-                </li>
-              </ul>
-                <h3>Traits</h3>
-                <div className='text' id='traits' dangerouslySetInnerHTML={{__html:props.Traits}}></div>
-                <h3>Actions</h3>
-                <div className='text' dangerouslySetInnerHTML={{__html:props.Actions}}></div>
-                <h3>Legendary Actions</h3>
-                <div className='text' dangerouslySetInnerHTML={{__html:props['Legendary Actions']}}></div>
+
             </div>
-          </div>
+
+            <div className='details grow'>
+              <p className='grow'>Armor Class: {props['Armor Class']}</p>
+              <p className='grow'>Hit Points: {props['Hit Points']}</p>
+              <p className='grow'>Speed: {props.Speed}</p>
+            </div>
+
+            <div className='content'>
+              <div className='stats grow'>
+                <ul>
+                  <li className='grow'>
+                    <p>STR: {props.STR} - {props.STR_mod}</p>
+                  </li>
+                  <li className='grow'>
+                    <p>DEX: {props.DEX} - {props.DEX_mod}</p>
+                  </li>
+                  <li className='grow'>
+                    <p>CON: {props.CON} - {props.CON_mod}</p>
+                  </li>
+                  <li className='grow'>
+                    <p>INT: {props.INT} - {props.INT_mod}</p>
+                  </li>
+                  <li className='grow'>
+                    <p>WIS: {props.WIS} - {props.WIS_mod}</p>
+                  </li>
+                  <li className='grow'>
+                    <p>CHA: {props.CHA} - {props.CHA_mod}</p>
+                  </li>
+                </ul>
+              </div>
+              <div className='body grow'>
+                <ul className='abilities'>
+                  <h3>Ablilities</h3>
+                  <li>
+                    <p>Languages: {props.Languages}</p>
+                  </li>
+                  <li>
+                    <p>Senses: {props.Senses}</p>
+                  </li>
+                  <li>
+                    <p>Skills: {props.Skills}</p>
+                  </li>
+                </ul>
+                  <h3>Traits</h3>
+                  <div className='text' id='traits' dangerouslySetInnerHTML={{__html:props.Traits}}></div>
+                  <h3>Actions</h3>
+                  <div className='text' dangerouslySetInnerHTML={{__html:props.Actions}}></div>
+                  <h3>Legendary Actions</h3>
+                  <div className='text' dangerouslySetInnerHTML={{__html:props['Legendary Actions']}}></div>
+              </div>
+            </div>
         </div>
     </div>
   );
-}
+});
